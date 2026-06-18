3a79fcc16b4c8e452dd0b50f639b24d11a5cec9e
shuco10 <afriasromero@gmail.com>
2026-06-18 17:21:45 +0200

    Redirect to index.html on logout instead of reload

--- public/auth.js
+++ public/auth.js
@@ -1,9 +1,10 @@
 // auth.js - Control global de permisos
+
 function esAdmin() {
     return localStorage.getItem('rol') === 'admin';
 }
 
 function cerrarSesion() {
     localStorage.removeItem('rol');
-    window.location.reload();
+    window.location.href = 'index.html'; 
 }
