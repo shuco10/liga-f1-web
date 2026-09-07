4722b5883631cce056582671fc8e33204680a1ff
shuco10 <afriasromero@gmail.com>
2026-09-07 16:29:53 +0200

    Add bcrypt dependency to server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const { Pool } = require('pg');
-
+const bcrypt = require('bcrypt');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
