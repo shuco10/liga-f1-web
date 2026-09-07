181bb2d6e954880e3092ea654a47e86a46fe5f11
shuco10 <afriasromero@gmail.com>
2026-09-07 17:13:59 +0200

    Add middleware and session configuration

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+
+
 // ==========================================
 // CONFIGURACIÓN DE MIDDLEWARES Y SESIONES
 // ==========================================
